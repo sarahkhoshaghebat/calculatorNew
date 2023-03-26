ddc49f60fe5a0d7d1c45f9964d63776e7d0bfee5
Gaël Thomas <sarah_khoshaghebat@yahoo.com>
2023-03-26 14:24:53 +0330

    int

--- script.js
+++ script.js
@@ -7,25 +7,115 @@ let btnMul = document.querySelector('#btn-Mul');
 let btnMinus = document.querySelector('#btn-Minus');
 let btnPlus = document.querySelector('#btn-Plus');
 let btnPn = document.querySelector('#btn-Pn');
-let Point = document.querySelector('#Point');
+let btnPoint = document.querySelector('#btnPoint');
 let btnEquls = document.querySelector('#btn-Equls');
 
-
+let setPoint = false;
+let number1, number2, result;
+let op = "";
+let setResult = false;
 let btnNumbers = document.querySelectorAll('.btn-number');
 
 
-
-btnClear.addEventListener('click',(event) =>{
-    display.textContent = "0.0";
+btnPoint.addEventListener('click',(e) =>{
+    if(setPoint == false){
+        display.textContent+= ".";
+        setPoint = true;
+    }
 });
 
-btnNumbers.forEach((item) => {
-    item.addEventListener('click',(event) => {
-        if(display.textContent == "0.0"){
-        display.textContent = "event.target.textContent";
-    } else {
-        display.textContent += "event.target.textContent";
+btnPn.addEventListener('click',(e) => {
+    display.textContent = display.textContent * -1;
+});
 
+
+btnClear.addEventListener('click',(e) => {
+    display.textContent = "0.0";
+    setPoint = false;
+    setResult = false;
+    number1 = 0;
+    number2 = 0;
+});
+
+
+
+btnBackSpace.addEventListener('click',(e) => {
+    let len = display.textContent.length;
+    let lastDigit = display.textContent.subster(len-1 ,len);
+    if( lastDigit =="."){
+        setPoint = false;
+    }
+    if (len >1 ) {
+    display.textContent = display.textContent.subster(0,len-1);
+    } else {
+        display.textContent = "0.0";
+    }
+})
+
+
+
+btnNumbers.forEach((item) => {
+    item.addEventListener('click',(e) => {
+        if(display.textContent == "0.0"){
+        display.textContent = "e.target.textContent";
+    } else {
+        display.textContent += "e.target.textContent";
     }
     })
+});
+
+
+
+btnPlus.addEventListener('click',(e) =>{
+    number1 = number(display.textContent);
+    display.textContent = "0.0";
+    op ="+";
+});
+
+
+btnClearLastAction.addEventListener('click',(e) =>{
+    display.textContent = 0.0;
+})
+
+
+btnEquls.addEventListener('click',(e) => {
+    if(setResult == false) {
+    number2 = number(display.textContent);
+    } else{
+        number1 = number(display.textContent);
+    }
+    switch(op){
+        case "+":
+        result = number1 +number2;
+        break;
+        case "-":
+        result = number1 -number2;
+        break;
+        case "*":
+        result = number1 *number2;
+        break;
+        case "/":
+        result = number1 /number2;
+        break;
+    }
+    display.textContent = result;
+    setResult = true;
+});
+
+btnMinus.addEventListener('click',(e) =>{
+    number1 = number(display.textContent);
+    display.textContent = "0.0";
+    op ="-";
+});
+
+btnDiv.addEventListener('click',(e) =>{
+    number1 = number(display.textContent);
+    display.textContent = "0.0";
+    op ="/";
+});
+
+btnMul.addEventListener('click',(e) =>{
+    number1 = number(display.textContent);
+    display.textContent = "0.0";
+    op ="*";
 });
